6060a22a6f78b071cfa77207ad5e4158ce5224f5
CynicalCake <esteban.vg00@gmail.com>
2023-04-15 20:10:30 -0400

    Some fixes

--- Exercises/Python/longer.py
+++ Exercises/Python/longer.py
@@ -3,6 +3,14 @@ Write a function that, given a list of words, returns the longest word.
 """
 
 def longer(list):
+    """Given a list of words, returns the longest word.
+
+    Args:
+        list (String): A list of words (String).
+
+    Returns:
+        String: Returns the longest word in the list.
+    """
     res = ""
     for i in list:
         if len(i) >= len(res):
